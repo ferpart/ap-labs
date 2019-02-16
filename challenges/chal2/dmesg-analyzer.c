033524ed2234c6509aa5ab0053e898f6e87acd5e
Fernando Partida Milanes <ferpart@hotmail.com>
2019-02-15 18:36:16 -0600

    dmesg can now categorize

--- challenges/chal2/dmesg-analyzer.c
+++ challenges/chal2/dmesg-analyzer.c
@@ -9,11 +9,16 @@
 #define ERROR 1
 #define SUCCESS 0
 
+#define maxlogLen 50
+#define logStart 15
+
 void analizeLog(char *logFile, char *report);
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 
-    if (argc < 2) {
+    if (argc < 2)
+    {
 	printf("Usage:./dmesg-analizer.o logfile.txt\n");
 	return 1;
     }
@@ -23,28 +28,84 @@ int main(int argc, char **argv) {
     return 0;
 }
 
-void analizeLog(char *logFile, char *report) {
-    printf("Generating Report from: [%s] log file\n", logFile);
+void analizeLog(char *logFile, char *report)
+{
+	printf("Generating Report from: [%s] log file\n", logFile);
 
-    // Implement your solution here.
+    	// Implement your solution here.
     
-    FILE * fp;
-    char * line = NULL;
-    size_t len = 0;
-    ssize_t read;
-
-    fp = fopen(logFile, "r");
-    if (fp == NULL)
-        exit(EXIT_FAILURE);
-
-    while ((read = getline(&line, &len, fp)) != -1) {
-    	//printf("Retrieved line of length %zu:\n", read);
-        printf("%s", line);
-	for (int i = 15; line[i-1]==':' && i!=45; i++){
-		printf("%s", line[i]);
-	}
-    }		
+    	int fd;
+	//char * line = NULL;
+	//size_t len = 0;
 	
+	fd = open(logFile, O_RDONLY);
+	if (fd == -1)
+       		exit(ERROR);
 
+	char *buffer, *temp;
+	buffer=calloc(nBytes, sizeof(char));
+	temp=calloc(maxlogLen, sizeof(char));
+	temp[nBytes]='\0';
+	buffer[maxlogLen]='\0';
+	//int i=0, 
+	int j=0,
+	    k=0, 
+	    flag=0,
+	    jump=0;
+    	
+	while (read(fd, buffer, nBytes-1) > 0)
+	{
+    		for (int i=0; buffer[i]!='\0'; i++)
+		{
+			//printf("%d num test\n", i );
+			//printf("%c\n", buffer[i+1]);
+			if (buffer[i]!='\n')
+			{
+				//printf("%c", buffer[i]);
+				if (i <=maxlogLen+logStart && i>=logStart && 
+						flag!=1)
+				{
+					//printf("test\n");
+					temp[j]=buffer[i];
+					//printf("%c\n", temp[j]);
+					if (buffer[i]==':' && (buffer[i+1]==' ' 
+							|| buffer[i+1]=='\n'))		
+					{
+							
+						flag=1;
+						printf("%s\n", temp);
+					}
+					j++;
+
+				}
+
+				/*if (flag)
+				{	
+					//printf("test");
+					printf("%s\n", temp);
+				}*/
+
+
+			}else
+			{
+				flag=0,
+				j=0;
+				
+				jump = (i*sizeof(char)-strlen(buffer)*sizeof(char))+1;
+				
+				lseek(fd, jump , SEEK_CUR);
+
+				memset(temp, '\0' ,maxlogLen);
+				
+				break;
+			}
+		}
+		
+		memset(buffer, '\0', nBytes);	
+    	}	
+	
+	close(fd);
     printf("Report is generated at: [%s]\n", report);
 }
+
+
