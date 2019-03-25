92500252c38921f4b7d6b06d0051bb9f458795f3
Fernando Partida Milanes <ferpart@hotmail.com>
2019-03-25 11:33:30 -0600

    logger library added

---
+++ labs/lab3.2/logger.h
@@ -0,0 +1,9 @@
+/*
+ * This is the library for the logger.c file.
+ */
+
+int initLogger(char *logType);
+int infof(const char *format, ...);
+int warnf(const char *format, ...);
+int errorf(const char *format, ...);
+int panicf(const char *format, ...);
