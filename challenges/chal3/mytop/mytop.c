a205e943c4876ea88e55eda25eee74a50686d2d2
Fernando Partida Milanes <ferpart@hotmail.com>
2019-03-28 11:38:26 -0600

    Indentation fixes and error fixing

--- challenges/chal3/mytop/mytop.c
+++ challenges/chal3/mytop/mytop.c
@@ -12,92 +12,93 @@
 #define nBytes 512
 
 void clear(),
-     topWriter(char *tempFile),
-     filePrinter(int fd),
-     interruptionWriter(int signal),
-     forceExit(int signal),
-     cleanup(void);
-     
+topWriter(char *tempFile),
+filePrinter(int fd),
+interruptionWriter(int signal), forceExit(int signal), cleanup(void);
+
 char *statusExplorer(char *path, int openFiles);
 
 int openFileExplorer(char *path);
 
-int main(){
-  // Place your magic here
-  
-  if (signal(SIGINT, interruptionWriter)== SIG_ERR)
-		printf("Signal map wasn't successful");
- 
-  if (signal(SIGQUIT, forceExit)== SIG_ERR)
+int main()
+{
+	// Place your magic here
+
+	if (signal(SIGINT, interruptionWriter) == SIG_ERR)
 		printf("Signal map wasn't successful");
 
-  clear();
-  while (1)
-  {
-  clear();
-  topWriter(".temp");
-  } 
- 
-  atexit(cleanup);
+	if (signal(SIGQUIT, forceExit) == SIG_ERR)
+		printf("Signal map wasn't successful");
 
-  return 0;
+	clear();
+	while (1) {
+		clear();
+		topWriter(".temp");
+	}
+
+	atexit(cleanup);
+
+	return 0;
 }
 
-void clear() {
-  printf("\e[1;1H\e[2J"); 
+void clear()
+{
+	printf("\e[1;1H\e[2J");
 }
 
-void topWriter(char *tempFile){
-	
+void topWriter(char *tempFile)
+{
+
 	DIR *dir;
 
 	struct dirent *ent;
 
-	char *procDir,
-	     *result;
+	char *procDir, *result;
 
 	int fd;
 
-	procDir=calloc(DIRSIZE, sizeof(char));
-	result=calloc(nBytes, sizeof(char));
-	
-	fd = open(tempFile , O_RDWR | O_CREAT, S_IRWXO);
+	procDir = calloc(DIRSIZE, sizeof(char));
+	result = calloc(nBytes, sizeof(char));
 
-	if (fd == -1)
-	{
+	fd = open(tempFile, O_RDWR | O_CREAT, S_IRWXO);
+
+	if (fd == -1) {
 		close(fd);
 		printf("Couldn't create file");
 		exit(EXIT_FAILURE);
 	}
-	sprintf(result , "____________________________________________________________________________________________________________________________________\n");
+	sprintf(result,
+		"____________________________________________________________________________________________________________________________________\n");
 	write(fd, result, strlen(result));
 	//result[0]='\0';
- 	sprintf(result, "|  %-9s|  %-9s|  %-41s|  %-15s|  %-15s|  %-9s|  %-12s|\n", "PID", "Parent", "Name", "State", "Memory", "#Threads", "Open Files");
+	sprintf(result,
+		"|  %-9s|  %-9s|  %-41s|  %-15s|  %-15s|  %-9s|  %-12s|\n",
+		"PID", "Parent", "Name", "State", "Memory", "#Threads",
+		"Open Files");
 	write(fd, result, strlen(result));
 
-  	sprintf(result, "|-----------|-----------|-------------------------------------------|-----------------|-----------------|-----------|--------------|\n");
+	sprintf(result,
+		"|-----------|-----------|-------------------------------------------|-----------------|-----------------|-----------|--------------|\n");
 	write(fd, result, strlen(result));
 
 	strcpy(procDir, "/proc");
 
 	dir = opendir(procDir);
 
-	if (dir != NULL)
-	{
-		while ((ent = readdir(dir))!=NULL)
-		{
-			if(isdigit (ent->d_name[0]))
-			{
+	if (dir != NULL) {
+		while ((ent = readdir(dir)) != NULL) {
+			if (isdigit(ent->d_name[0])) {
 				strcpy(procDir, "/proc/");
 				strcat(procDir, ent->d_name);
-				
-				sprintf(result,"%s", statusExplorer(procDir,
-							 openFileExplorer(procDir)));
-				if (strlen(result)>100)
+
+				sprintf(result, "%s", statusExplorer(procDir,
+								     openFileExplorer
+								     (procDir)));
+				if (strlen(result) > 100)
 					write(fd, result, strlen(result));
 
 				memset(procDir, '\0', DIRSIZE);
-				memset(result, '\0', nBytes);	
+				memset(result, '\0', nBytes);
 			}
 
 		}
@@ -105,40 +106,30 @@ void topWriter(char *tempFile){
 	}
 	closedir(dir);
 
-	sprintf(result, "‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\n");
+	sprintf(result,
+		"‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\n");
 	write(fd, result, strlen(result));
-	
+
 	filePrinter(fd);
 
 	close(fd);
 
 }
 
-char *statusExplorer(char *path, int openFiles){
-	
-	char *finalFile,
-	     *buffer,
-	     *child,
-	     *status,
-	     *returnValue;
+char *statusExplorer(char *path, int openFiles)
+{
 
-	char *pid,
-	     *ppid,
-	     *name,
-	     *state,
-	     *memory,
-	     *threads;
+	char *finalFile, *buffer, *child, *status, *returnValue;
 
-	int fd,
-	    flag,
-	    jump,
-	    j=0;
+	char *pid, *ppid, *name, *state, *memory, *threads;
 
-	double mbs=0;
+	int fd, flag, jump, j = 0;
 
-	finalFile = calloc(DIRSIZE+7, sizeof(char));
+	double mbs = 0;
+
+	finalFile = calloc(DIRSIZE + 7, sizeof(char));
 	buffer = calloc(nBytes, sizeof(char));
-	child = calloc(8, sizeof(char)); 
+	child = calloc(8, sizeof(char));
 	status = calloc(13, sizeof(char));
 
 	pid = calloc(6, sizeof(char));
@@ -152,87 +143,86 @@ char *statusExplorer(char *path, int openFiles){
 
 	strcpy(finalFile, path);
 	strcat(finalFile, "/status");
-	
-	 
+
 	fd = open(finalFile, O_RDONLY);
-	
-	if (fd == -1)
-	{
+
+	if (fd == -1) {
 		close(fd);
 		return "";
 	}
 
-	while (read(fd, buffer, nBytes-1) > 0)
-	{
+	while (read(fd, buffer, nBytes - 1) > 0) {
 		flag = 0;
-		for (int i=0; buffer[i]!='\0'; i++)
-		{
-			if (buffer[i]!='\n')
-			{
-				if(buffer[i] != ':' && flag==0)
-				{
-					child[i]=buffer[i];
-				}else if (buffer[i] == ':')
-				{
-					flag=1;
+		for (int i = 0; buffer[i] != '\0'; i++) {
+			if (buffer[i] != '\n') {
+				if (buffer[i] != ':' && flag == 0) {
+					child[i] = buffer[i];
+				} else if (buffer[i] == ':') {
+					flag = 1;
 				}
 
-				if (flag)
-				{
-					if(buffer[i]!=':' && buffer[i]!='\t')
-					{
-						if (strcmp(child, "Pid")==0 ||
-						    strcmp(child, "PPid")==0 ||
-						    strcmp(child, "Name")==0 ||
-						    strcmp(child, "State")==0 ||
-						    strcmp(child, "Threads")==0 ||
-						    strcmp(child, "VmSize")==0 )
-						{
-							status[j]=buffer[i];
+				if (flag) {
+					if (buffer[i] != ':'
+					    && buffer[i] != '\t') {
+						if (strcmp(child, "Pid") == 0
+						    || strcmp(child,
+							      "PPid") == 0
+						    || strcmp(child,
+							      "Name") == 0
+						    || strcmp(child,
+							      "State") == 0
+						    || strcmp(child,
+							      "Threads") == 0
+						    || strcmp(child,
+							      "VmSize") == 0) {
+							status[j] = buffer[i];
 							j++;
 						}
 
 					}
-					if(buffer[i+1] == '\n')
-					{
-						if(strcmp(child, "Pid")==0)
-						{
+					if (buffer[i + 1] == '\n') {
+						if (strcmp(child, "Pid") == 0) {
 							strcpy(pid, status);
-							//printf("%-10s", pid);	
-						}else if(strcmp(child, "PPid") ==0)
-						{
+							//printf("%-10s", pid); 
+						} else if (strcmp(child, "PPid")
+							   == 0) {
 							strcpy(ppid, status);
 							//printf("%s\n", ppid);
-						}else if(strcmp(child, "Name") ==0)
-						{
+						} else if (strcmp(child, "Name")
+							   == 0) {
 							strcpy(name, status);
-						}else if(strcmp(child, "State") ==0)
-						{
+						} else
+						    if (strcmp(child, "State")
+							== 0) {
 							strcpy(state, status);
-						}else if(strcmp(child, "VmSize") ==0)
-						{
-							mbs = (atoi(status)+.0)/1000;
-							sprintf(memory ,"%.4fM",  mbs);
-						}else if(strcmp(child, "Threads" )==0)
-						{
+						} else
+						    if (strcmp(child, "VmSize")
+							== 0) {
+							mbs =
+							    (atoi(status) +
+							     .0) / 1000;
+							sprintf(memory, "%.4fM",
+								mbs);
+						} else
+						    if (strcmp(child, "Threads")
+							== 0) {
 							strcpy(threads, status);
 						}
-						flag=0;
+						flag = 0;
 						memset(status, '\0', 13);
 					}
-				}	
-			}else
-			{
-				jump = (i*sizeof(char)-strlen(buffer)*
-						sizeof(char))+1;
-				
-				j=0;
+				}
+			} else {
+				jump = (i * sizeof(char) - strlen(buffer) *
+					sizeof(char)) + 1;
 
-				lseek(fd, jump , SEEK_CUR);
-				
+				j = 0;
+
+				lseek(fd, jump, SEEK_CUR);
+
 				memset(child, '\0', 8);
 				memset(buffer, '\0', nBytes);
-			}		
+			}
 		}
 	}
 
@@ -240,9 +230,11 @@ char *statusExplorer(char *path, int openFiles){
 	if (memory[0] == '\0')
 		strcpy(memory, "0.000M");
 
-	sprintf(returnValue, "|  %-9s|  %-9s|  %-41s|  %-15s|  %-15s|  %-9s|  %-12d|\n", pid, ppid, name, state, memory, threads, openFiles);
+	sprintf(returnValue,
+		"|  %-9s|  %-9s|  %-41s|  %-15s|  %-15s|  %-9s|  %-12d|\n", pid,
+		ppid, name, state, memory, threads, openFiles);
 
-	return returnValue;	
+	return returnValue;
 }
 
 int openFileExplorer(char *path)
@@ -253,29 +245,26 @@ int openFileExplorer(char *path)
 
 	char *procDir;
 
-	int dirs=0;
+	int dirs = 0;
 
-	procDir=calloc(DIRSIZE, sizeof(char));
-	
+	procDir = calloc(DIRSIZE, sizeof(char));
+
 	strcpy(procDir, path);
 	strcat(procDir, "/fd");
 
 	dirE = opendir(procDir);
 
-	if (dirE != NULL)
-	{
-		while ((ent = readdir(dirE))!=NULL)
-		{
-			if(isdigit (ent->d_name[0]))
-			{
+	if (dirE != NULL) {
+		while ((ent = readdir(dirE)) != NULL) {
+			if (isdigit(ent->d_name[0])) {
 				dirs++;
 			}
 
 		}
 
 	}
-	closedir(dirE);	
-	
+	closedir(dirE);
+
 	return dirs;
 }
 
@@ -283,30 +272,31 @@ void filePrinter(int fd)
 {
 	char *buffer;
 
-	buffer=calloc(nBytes, sizeof(char));
-	
-	lseek(fd, 0 , SEEK_SET);
+	buffer = calloc(nBytes, sizeof(char));
 
-	while (read(fd, buffer, nBytes-1)>0)
-	{
+	lseek(fd, 0, SEEK_SET);
+
+	while (read(fd, buffer, nBytes - 1) > 0) {
 		printf("%.*s", (int)strlen(buffer), buffer);
 		memset(buffer, '\0', nBytes);
 	}
-	
+
 }
 
 void interruptionWriter(int signal)
 {
-	char date[11],
-	     fileTitle[25];
-	
+	char date[11], fileTitle[25];
+
 	time_t t = time(NULL);
 	struct tm tm = *localtime(&t);
 
 	sprintf(date, "%d-%d-%d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday);
-	
+
 	strcpy(fileTitle, "mytop-status-");
 	strcat(fileTitle, date);
+
+	topWriter(fileTitle);
+
 }
 
 void forceExit(int signal)
@@ -319,4 +309,3 @@ void cleanup(void)
 {
 	remove(".temp");
 }
-
